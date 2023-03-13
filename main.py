d012d662ed0222860b9cb072028c01d837dfe55c
Vitlyr <103600474+Vitlyr@users.noreply.github.com>
2023-03-13 17:10:58 -0400

    Update main.py v1.0

--- main.py
+++ main.py
@@ -1,26 +1,23 @@
-# This example requires the 'message_content' privileged intents
-
 import os
 import discord
 from discord.ext import commands
-
-
 intents = discord.Intents.default()
 intents.message_content = True
 bot = commands.Bot(command_prefix='!', intents=intents)
-
-
 @bot.event
 async def on_ready():
     print(f"Logged in as {bot.user}")
-
+    
 @bot.command()
 async def ping(ctx):
-    await ctx.send('pong')
+    embed = discord.Embed(title="🏓 Pong!", color=0xC9E358)
+    embed.set_footer(text="This was sent by the Developer Team Bot!")
+    await ctx.send(embed=embed)
 
 @bot.command()
 async def hello(ctx):
-    await ctx.send("Choo choo! 🚅")
-
-
+    user = ctx.author
+    embed = discord.Embed(title="👋 Hola!", descriptionf="Hello {user.display_name}!", color=0xFF5733)
+    embed.set_footer(text="This was sent by the Developer Team Bot!")
+    await ctx.send(embed=embed)
 bot.run(os.environ["DISCORD_TOKEN"])
